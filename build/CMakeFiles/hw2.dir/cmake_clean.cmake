f1f6183093d996924de8e15c1c003b524dd0861f
andrey-kireev-1 <vrk2803@yandex.ru>
2021-11-09 18:45:27 +0300

    travis

---
+++ build/CMakeFiles/hw2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw2.dir/project/src/main.c.o"
+  "hw2"
+  "hw2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
